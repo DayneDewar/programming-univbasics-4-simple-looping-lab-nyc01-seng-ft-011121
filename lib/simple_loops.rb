8194bd6d2fb7f166025951feca61d25933c83c5b
Dayne Dewar <daynedewar@yahoo.com>
2020-12-27 00:54:18 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -31,7 +31,7 @@ def return_string_array(array)
   new_array = Array.new
   
    while counter < array.length do
-     array[counter].to_s
+     new_array.push(array[counter].to_s)
      counter += 1
    end
 end
